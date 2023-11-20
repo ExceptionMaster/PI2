2fa0b0092c1e7384262f356be6c668f7a9eaf5ef
Jose <jomaamga@outlook.es>
2023-11-21 00:26:09 +0100

    IterBigInteger

--- src/main/java/adda/tests/TestEjercicio1.java
+++ src/main/java/adda/tests/TestEjercicio1.java
@@ -54,11 +54,11 @@ public class TestEjercicio1 {
 	}
 	
 	public static void genDataIBI() {
-		Integer nMin = 100; // n mínimo para el cálculo
-		Integer nMax = 5000; // n máximo para el cálculo
-		Integer nIncr = 333; // incremento en los valores de n
-		Integer nIter = 100; // número de iteraciones para cada medición de tiempo
-		Integer nIterWarmup = 1000; // número de iteraciones para warmup
+		Integer nMin = 100; // Valor mínimo de n
+	    Integer nMax = 1000; // Valor máximo de n
+	    Integer nIncr = 33; // Incremento en los valores de n
+	    Integer nIter = 2000; // Número de iteraciones para cada medición de tiempo
+	    Integer nIterWarmup = 1000; // Número de iteraciones para warmup
 		
 		String file = "ficheros_generados/ibi.txt";
 		Function<Integer,Long> f1 = GenData.time(t -> Ejercicio1.fIterBigInteger(t));
@@ -115,5 +115,7 @@ public class TestEjercicio1 {
 		MatPlotLib.showCombined("Comparación", List.of("ficheros_generados/rd.txt","ficheros_generados/rbi.txt",
 				"ficheros_generados/id.txt","ficheros_generados/ibi.txt"), 
 				List.of("Recursiva Double","Recursiva BigInteger","Iterativa Double","Iterativa BigInteger"));
+		
+		
 	}
 }
