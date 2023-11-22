d39116b40fb519e6649ee0ce51cee2a33200bc35
Jose <jomaamga@outlook.es>
2023-11-22 22:57:57 +0100

    fin

--- src/main/java/adda/ejercicios/Ejercicio4.java
+++ src/main/java/adda/ejercicios/Ejercicio4.java
@@ -9,20 +9,32 @@ import us.lsi.tiposrecursivos.BinaryTree;
 import us.lsi.tiposrecursivos.BinaryTree.BEmpty;
 import us.lsi.tiposrecursivos.BinaryTree.BLeaf;
 import us.lsi.tiposrecursivos.BinaryTree.BTree;
+import us.lsi.tiposrecursivos.Tree;
+import us.lsi.tiposrecursivos.Tree.TEmpty;
+import us.lsi.tiposrecursivos.Tree.TLeaf;
+import us.lsi.tiposrecursivos.Tree.TNary;
 
 public class Ejercicio4 {		
-	public static List<List<Integer>> ej4Aux(BinaryTree<Integer> tree) {
+	public static List<List<Integer>> ej4Bin(BinaryTree<Integer> tree) {
+		Function<List<Integer>,Integer> suma = ls -> ls.stream().collect(Collectors.summingInt(i -> i + (i+1)));
+		List<List<Integer>> res = ej4BinAux(tree).stream()
+				.filter(path -> suma.apply(path) % path.size() == 0)
+				.collect(Collectors.toList());
+		return res;
+	}
+		
+	public static List<List<Integer>> ej4BinAux(BinaryTree<Integer> tree) {
 		return switch (tree) {
 			case BEmpty<Integer> t -> List.of();
 			case BLeaf<Integer> t -> List.of(new ArrayList<>(List.of(t.optionalLabel().get())));
 			case BTree<Integer> t -> {
 				List<List<Integer>> res = new ArrayList<>();
-				for (List<Integer> path : ej4Aux(t.left())) {
+				for (List<Integer> path : ej4BinAux(t.left())) {
 					List<Integer> newPath = new ArrayList<>(path);
 					newPath.add(0, t.optionalLabel().get());
 					res.add(newPath);
 				}
-				for (List<Integer> path : ej4Aux(t.right())) {
+				for (List<Integer> path : ej4BinAux(t.right())) {
 					List<Integer> newPath = new ArrayList<>(path);
 					newPath.add(0, t.optionalLabel().get());
 					res.add(newPath);
@@ -32,10 +44,29 @@ public class Ejercicio4 {
 		};
 	}
 
-	public static List<List<Integer>> ej4(BinaryTree<Integer> tree) {
-		Function<List<Integer>,Integer> suma = ls -> ls.stream().collect(Collectors.summingInt(i -> i + (i+1)));
-		List<List<Integer>> res = ej4Aux(tree).stream().filter(path -> suma.apply(path) % path.size() == 0)
-				.collect(Collectors.toList());
-		return res;
-	}
+	public static List<List<Integer>> ej4Nary(Tree<Integer> tree) {
+        Function<List<Integer>, Integer> suma = ls -> ls.stream().mapToInt(i -> i + (i + 1)).sum();
+        List<List<Integer>> res = ej4NaryAux(tree, suma).stream()
+                .filter(path -> suma.apply(path) % path.size() == 0)
+                .collect(java.util.stream.Collectors.toList());
+        return res;
+    }
+
+    private static List<List<Integer>> ej4NaryAux(Tree<Integer> tree, Function<List<Integer>, Integer> suma) {
+        return switch (tree) {
+            case TEmpty<Integer> t -> List.of();
+            case TLeaf<Integer> t -> List.of(new ArrayList<>(List.of(t.optionalLabel().get())));
+            case TNary<Integer> t -> {
+                List<List<Integer>> res = new ArrayList<>();
+                for (Tree<Integer> child : t.children()) {
+                    List<List<Integer>> childPaths = ej4NaryAux(child, suma);
+                    for (List<Integer> path : childPaths) {
+                        path.add(0, t.optionalLabel().get());
+                        res.add(new ArrayList<>(path));
+                    }
+                }
+                yield res;
+            }
+        };
+    }
 }
--- src/main/java/adda/tests/TestEjercicio4.java
+++ src/main/java/adda/tests/TestEjercicio4.java
@@ -5,13 +5,27 @@ import java.util.List;
 import adda.ejercicios.Ejercicio4;
 import us.lsi.common.Files2;
 import us.lsi.tiposrecursivos.BinaryTree;
+import us.lsi.tiposrecursivos.Tree;
 
 public class TestEjercicio4 {
 	
 	public static void testBinary() {
+		System.out.println("BINARIO:");
 	    for(String linea : Files2.linesFromFile("data/PI2Ej4DatosEntradaBinary.txt")) {
 	        BinaryTree<Integer> t = BinaryTree.parse(linea, Integer::parseInt);
-	        List<List<Integer>> caminos = Ejercicio4.ej4(t);
+	        List<List<Integer>> caminos = Ejercicio4.ej4Bin(t);
+	        System.out.println("Arbol: " + linea);
+	        for(List<Integer> camino : caminos) {
+	            System.out.println("    " + camino);
+	        }
+	    }
+	}
+	
+	public static void testNary() {
+		System.out.println("\n\nN-ARIO:");
+	    for(String linea : Files2.linesFromFile("data/PI2Ej4DatosEntradaNary.txt")) {
+	        Tree<Integer> t = Tree.parse(linea, Integer::parseInt);
+	        List<List<Integer>> caminos = Ejercicio4.ej4Nary(t);
 	        System.out.println("Arbol: " + linea);
 	        for(List<Integer> camino : caminos) {
 	            System.out.println("    " + camino);
@@ -22,5 +36,6 @@ public class TestEjercicio4 {
 	
 	public static void test() {
 		testBinary();
+		testNary();
 	}
 }
